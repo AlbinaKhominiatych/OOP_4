da4e54d7f941335c0e4e480f6523d1fdb1746cdc
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-08 20:21:56 +0200

    Сценарій 1: Якщо користувач хоче додати числа, які не є цілими, то потрібно скористатися округленням до цілих чисел перед сумуванням.

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ class Calculator:
     @staticmethod
     def add_numbers(num1, num2):
         return num1 + num2
+    #сценарій 1
+    @classmethod
+    def add_numbers_rounded(cls, num1, num2):
+        rounded_num1 = round(num1)
+        rounded_num2 = round(num2)
+        return cls.add_numbers(rounded_num1, rounded_num2)
 
-print(Calculator.add_numbers(2, 3))
+print(Calculator.add_numbers(2.7, 3.4))
+print(Calculator.add_numbers_rounded(2.7, 3.4))
